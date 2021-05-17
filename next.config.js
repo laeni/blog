f7d0139023246f0691b5b738f47078b90328c36e
Laeni <m@laeni.cn>
2021-05-17 17:25:41 +0800

    fix：解决静态导出后404问题

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
     // 需要执行'next export'导出为静态页面时使用,目的是将图片的cdn地址设置为和本站一致（静态导出将失去图片优化）
     images: { loader: 'imgix', path: '/' },
+    future: {
+        webpack5: true
+    },
+    exportTrailingSlash: true
 }
