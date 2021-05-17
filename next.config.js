cb6957abd4c026082834f77a11ea03aed1b87a0d
Laeni <m@laeni.cn>
2021-05-17 21:42:35 +0800

    perf：优化代码块显示效果

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 module.exports = {
     // 需要执行'next export'导出为静态页面时使用,目的是将图片的cdn地址设置为和本站一致（静态导出将失去图片优化）
     images: { loader: 'imgix', path: '/' },
+    // 尾斜杠 - 将'/xxx.html'输出为'/xxx/index.html',以解决导出静态文件后直接访问子页面404的问题
+    trailingSlash: true,
     future: {
         webpack5: true
     },
-    exportTrailingSlash: true
 }
--- pages/[...path].tsx
+++ pages/[...path].tsx
@@ -5,31 +5,77 @@ import { GetStaticProps, GetStaticPaths } from 'next'
 import {rootTitle} from "./_document";
 import styles from "./[...path].module.scss"
 import ReactMarkdown from 'react-markdown'
-// import SyntaxHighlighter from 'react-syntax-highlighter';
-// import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-// import {docco} from 'react-syntax-highlighter/dist/cjs/styles/hljs'
 import { Components } from "react-markdown/src/ast-to-react";
 
+// import SyntaxHighlighter from 'react-syntax-highlighter';
+// import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-// import java from 'react-syntax-highlighter/dist/cjs/languages/prism/java';
 
-import prism from 'react-syntax-highlighter/dist/cjs/styles/prism/prism'
+// 支持多种主题
+// import {
+//   coyWithoutShadows, coy, prism, solarizedlight, coldarkCold,                     // 浅色
+//   xonokai, vscDarkPlus, tomorrow, okaidia, materialOceanic, darcula, cb, a11yDark // 深色
+// } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import theme from 'react-syntax-highlighter/dist/cjs/styles/prism/prism'
 
-SyntaxHighlighter.registerLanguage('java', require('refractor/lang/java'));
+/// region 为了减小包大小，这里仅仅注册常见的语言 - https://github.com/react-syntax-highlighter/react-syntax-highlighter#light-build
+import bash from 'refractor/lang/bash';
+import basic from 'refractor/lang/basic';
+import c from 'refractor/lang/c';
+import cpp from 'refractor/lang/cpp';
+import css from 'refractor/lang/css';
+import go from 'refractor/lang/go';
+import java from 'refractor/lang/java';
+import javascript from 'refractor/lang/javascript';
+import json from 'refractor/lang/json';
+import jsx from 'refractor/lang/jsx';
+import kotlin from 'refractor/lang/kotlin';
+import less from 'refractor/lang/less';
+import lua from 'refractor/lang/lua';
+import makefile from 'refractor/lang/makefile';
+import markdown from 'refractor/lang/markdown';
+import nginx from 'refractor/lang/nginx';
+import python from 'refractor/lang/python';
+import sql from 'refractor/lang/sql';
+import typescript from 'refractor/lang/typescript';
+import yaml from 'refractor/lang/yaml';
+SyntaxHighlighter.registerLanguage('bash', bash);
+SyntaxHighlighter.registerLanguage('basic', basic);
+SyntaxHighlighter.registerLanguage('c', c);
+SyntaxHighlighter.registerLanguage('cpp', cpp);
+SyntaxHighlighter.registerLanguage('css', css);
+SyntaxHighlighter.registerLanguage('go', go);
+SyntaxHighlighter.registerLanguage('java', java);
+SyntaxHighlighter.registerLanguage('javascript', javascript);
+SyntaxHighlighter.registerLanguage('json', json);
+SyntaxHighlighter.registerLanguage('jsx', jsx);
+SyntaxHighlighter.registerLanguage('kotlin', kotlin);
+SyntaxHighlighter.registerLanguage('less', less);
+SyntaxHighlighter.registerLanguage('lua', lua);
+SyntaxHighlighter.registerLanguage('makefile', makefile);
+SyntaxHighlighter.registerLanguage('markdown', markdown);
+SyntaxHighlighter.registerLanguage('nginx', nginx);
+SyntaxHighlighter.registerLanguage('python', python);
+SyntaxHighlighter.registerLanguage('sql', sql);
+SyntaxHighlighter.registerLanguage('typescript', typescript);
+SyntaxHighlighter.registerLanguage('yaml', yaml);
+/// endregion
 
 const components: Components = {
   code({node, inline, className, children, ...props}) {
     const match = /language-(\w+)/.exec(className || '')
-    return !inline && match ? (
+
+    if (!inline && match) {return (
         <SyntaxHighlighter
             language={match[1]}
-            style={prism}
+            style={theme}
             children={String(children).replace(/\n$/, '')}
             {...props}
         />
-    ) : (
-        <code className={className} {...props} />
     )
+    } else {
+      return <code className={className} {...props} />;
+    }
   }
 }
 
@@ -50,7 +96,7 @@ export default function Post({ postData }: { postData: PostsContent }) {
         <div className="border-b-2 pt-4 mx-1"/>
       </div>
       <div className={styles.content}>
-        <ReactMarkdown components={components} children={postData.content} />
+        <ReactMarkdown components={components} children={postData.content} skipHtml/>
       </div>
     </article>
   </Layout>
@@ -65,7 +111,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  console.log('[...path].tsx ------------------------------------------ getStaticProps')
   const postData: PostsContent = await getPostData(params.path)
   return {
     props: { postData }
