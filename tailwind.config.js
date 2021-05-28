c10fdd8f60b0eb883520b54f772c33c59311e562
Laeni <m@laeni.cn>
2021-05-28 09:31:58 +0800

    feat：支持自动跟随系统明暗模式(实际效果尚未支持)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
   ],
-  darkMode: false, // or 'media' or 'class'
+  // 暗模式： https://tailwindcss.com/docs/dark-mode#basic-usage
+  darkMode: 'media',  // false or 'media' or 'class'
   theme: {
     extend: {},
   },
