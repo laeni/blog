9de3e3261a33a1d6ede3f4080326e1581f6b96f9
Laeni <m@laeni.cn>
2021-05-28 09:31:00 +0800

    feat：添加导航进度条

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import './globals.css'
 import {AppProps} from "next/app";
 import Head from "next/head";
+import { useRouter } from 'next/router'
+import NProgress from "nprogress";
+import "../lib/nprogress.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+
+    if (router?.events?.on) {
+        router.events.on('routeChangeStart', NProgress.start);
+        router.events.on('routeChangeComplete', NProgress.done);
+        router.events.on('routeChangeError', NProgress.done);
+    }
+
     return (
         <>
             <Head>
