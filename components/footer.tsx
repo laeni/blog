34f2b1a497cd986aead61588fc7e5f5f905fea0b
Laeni <m@laeni.cn>
2021-06-05 11:52:09 +0800

    feat：增加RSS

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,16 @@
-import Link from 'next/link'
-
 /**
  * 页脚.
  */
 export default function Footer() {
   return (
-    <div className="flex-none relative z-50 leading-3 ring-1 ring-gray-900 ring-opacity-5 shadow-sm py-6">
-      <div className="text-center px-2 text-xs sm:text-sm text-gray-400">
-        <span>© 2020-{new Date().getFullYear()} All Right Reserved <a target="_black" href="https://beian.miit.gov.cn/">滇ICP备17005647号-2</a></span>
+    <div className="flex-none relative z-50 leading-3 ring-1 ring-gray-900 ring-opacity-5 shadow-sm py-6 text-xs sm:text-sm text-gray-400">
+      <div className="flex justify-center">
+        <div className="px-2">
+          <span>© 2020-{new Date().getFullYear()} All Right Reserved <a target="_black" href="https://beian.miit.gov.cn/">滇ICP备17005647号-2</a></span>
+        </div>
+        <div className="px-2">
+          <a href="/feed.xml" target="_blank">RSS</a>
+        </div>
       </div>
     </div>
   )
