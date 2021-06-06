4e8cc87fa05704e97751c00338bab1b4394a7f56
Laeni <m@laeni.cn>
2021-06-06 17:00:30 +0800

    feat：增加百度站内工具

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,44 @@
-import './globals.css'
-import { AppProps } from "next/app";
-import Head from "next/head";
-import { useRouter } from 'next/router'
+import './globals.css';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { withRouter } from 'next/router';
 import NProgress from "nprogress";
-import "../lib/nprogress.css";
+import '../lib/nprogress.css';
+import React from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
+export default withRouter(class App extends React.Component<AppProps, any> {
+  componentDidMount() {
+    const { router } = this.props;
 
-  if (router?.events?.on) {
-    router.events.on('routeChangeStart', NProgress.start);
-    router.events.on('routeChangeComplete', NProgress.done);
-    router.events.on('routeChangeError', NProgress.done);
+    // 导航进度条
+    if (router?.events?.on) {
+      router.events.on('routeChangeStart', NProgress.start);
+      router.events.on('routeChangeComplete', NProgress.done);
+      router.events.on('routeChangeError', NProgress.done);
+    }
+    // 百度自动提交
+    (function(){
+      const bp = document.createElement('script');
+      if (window.location.protocol.split(':')[0] === 'https'){
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+      } else{
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+      }
+      const s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(bp, s);
+    })();
   }
 
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
-}
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        </Head>
+        <Component {...pageProps} />
+      </>
+    )
+  }
+})
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,7 @@ export default class MyDocument extends Document {
           <meta property="og:image" content={`https://og-image.vercel.app/${encodeURI(rootTitle)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`} />
           <meta name="og:title" content={rootTitle} />
           <meta name="twitter:card" content="summary_large_image" />
+          <meta name="baidu-site-verification" content="code-5a8bwoW7Pk" />
           <script src="/font_2464095_8m8uychzspt.js" />
         </Head>
         <body className="antialiased bg-gray-50 dark:bg-gray-900 text-gray-600 dark:text-gray-300">
