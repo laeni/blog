d32e6c653a110fe3d2468cb85b8302d62daade84
Laeni <m@laeni.cn>
2021-08-13 01:20:01 +0800

    fix：升级next版本并修正配置项解决打包失败问题

--- next.config.js
+++ next.config.js
@@ -3,7 +3,5 @@ module.exports = {
     images: { loader: 'imgix', path: '/' },
     // 尾斜杠 - 将'/xxx.html'输出为'/xxx/index.html',以解决导出静态文件后直接访问子页面404的问题
     trailingSlash: true,
-    future: {
-        webpack5: true
-    },
+    webpack5: true,
 }
