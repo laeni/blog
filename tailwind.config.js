2ef3d2a01a2605fe00bd395d2e7bd2d29c19b63c
Laeni <m@laeni.cn>
2021-12-14 22:04:58 +0800

    feat: 將 tailwindcss 和 next 版本升级到最新版

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 module.exports = {
-  purge: [
+  content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   // 暗模式： https://tailwindcss.com/docs/dark-mode#basic-usage
   darkMode: 'media',  // false or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [
